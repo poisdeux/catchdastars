0685d507fba14b83a5302860b0ffea4286017e02
Martijn Brekhof <m.brekhof@gmail.com>
2013-05-19 08:43:54 +0200

    * Refactor: added animationController to control animation flow

--- my-gdx-game/src/com/strategames/ui/LevelCompleteDialog.java
+++ my-gdx-game/src/com/strategames/ui/LevelCompleteDialog.java
@@ -41,14 +41,11 @@ public class LevelCompleteDialog implements ChalkLineAnimationListener {
 	private int delayCount;
 	private Stage stage;
 	private int totalScore;
+	private Label totalScoreLabel;
 	
-	
-	private enum animStates {
-		NONE, CHALKLINE_DRAW_BAR, CHALKLINE_CROSS_DRAW_VERTICAL, CHALKLINE_CROSS_DRAW_HORIZONTAL
-	}
-	
-	private animStates animState;
-	
+	private int animationPhase;
+	private Vector2 animPosition;
+
 	/**
 	 * Shows a scoreboard animation
 	 * @param game
@@ -59,9 +56,10 @@ public class LevelCompleteDialog implements ChalkLineAnimationListener {
 		this.skin = skin;
 		this.game = game;
 		this.scoreItems = new ArrayList<LevelCompleteDialog.ScoreItem>();
-		this.animState = animStates.NONE;
+		this.animPosition = new Vector2();
 		this.totalScore = currentScore;
 		this.chalkLines = new ArrayList<ChalkLine>();
+		this.totalScoreLabel = new Label(String.valueOf(this.totalScore), skin);
 	}
 
 	public void add(Image image, int amount, int scorePerGameObject) {
@@ -76,12 +74,12 @@ public class LevelCompleteDialog implements ChalkLineAnimationListener {
 	public void show(Stage stage) {
 		this.stage = stage;
 		this.rowHeight = this.maxRowHeight + this.padding;
+		this.animationPhase = -1;
+		setupUI();
+		animationController();
+	}
 
-		if( scoreItems.size() > 0 ) {
-			this.top = Gdx.graphics.getHeight() - (int) ( this.rowHeight );
-			showScoreItem(0);
-		}
-
+	private void setupUI() {
 		final Table table = new Table();
 		table.setFillParent(true);
 		table.bottom();
@@ -124,13 +122,57 @@ public class LevelCompleteDialog implements ChalkLineAnimationListener {
 		stage.addActor(table);
 	}
 
+	private void animationController() {
+		this.animationPhase++;
+		switch(this.animationPhase) {
+		case 0:
+			if( scoreItems.size() > 0 ) {
+				this.top = Gdx.graphics.getHeight() - (int) ( this.rowHeight );
+				showScoreItem(0);
+			}
+			break;
+		case 1:
+			this.animPosition.y -= 2 * padding;
+			this.animPosition.x = 50f;
+			ChalkLine line = ChalkLine.create(this.animPosition.x, 
+					this.animPosition.y, 
+					350f, 
+					this.animPosition.y, 420, LevelCompleteDialog.this);
+			stage.addActor(line);
+			Sounds.drawChalkLine.play();
+			break;
+		case 2:
+			this.animPosition.x = 350f + (this.padding * 2);
+			line = ChalkLine.create(this.animPosition.x, this.animPosition.y, 
+					this.animPosition.x + 50f, this.animPosition.y, 220, this);
+			this.stage.addActor(line);
+			Sounds.drawChalkLineShort2.play();
+			break;
+		case 3:
+			this.animPosition.x += 25f;
+			line = ChalkLine.create(this.animPosition.x, this.animPosition.y + 25f, 
+					this.animPosition.x, this.animPosition.y - 25f, 210, this);
+			this.stage.addActor(line);
+			Sounds.drawChalkLineShort1.play();
+			break;
+		case 4:
+			this.animPosition.x = 100f;
+			this.animPosition.y -= this.rowHeight;
+			showCashRegistry(this.animPosition.x, this.animPosition.y);
+			break;
+		case 5:
+			calculateTotalAnimation(0, this.animPosition.x, this.animPosition.y);
+			break;
+		}
+	}
+
 	private void showScoreItem(final int number) {
 		final ScoreItem scoreItem = this.scoreItems.get(number);
 
 		final int increment = scoreItem.getScorePerGameObject();
 		final int amount = scoreItem.getAmount() * increment;
 		final Sound sound = Sounds.getSoundForIncrement(increment);
-		
+
 		Table scoreItemTable = new Table();
 		scoreItemTable.setHeight(this.maxRowHeight);
 		scoreItemTable.add(scoreItem.getImageButton());
@@ -138,14 +180,16 @@ public class LevelCompleteDialog implements ChalkLineAnimationListener {
 		final Label label = new Label("", skin);
 		scoreItemTable.add(label).width(50);
 
-		scoreItemTable.setPosition(100f, -this.rowHeight);
-		final float finalYPosition = this.top - (this.rowHeight * number);
-				
+		this.animPosition.x = 100f;
+		this.animPosition.y = this.top - (this.rowHeight * number);
+
+		scoreItemTable.setPosition(animPosition.x, -this.rowHeight);
+
 		this.count = 0;
 		this.delayCount = 0;
-		
+
 		scoreItemTable.addAction(sequence(
-				moveTo(100f, finalYPosition, 1f, Interpolation.circleOut),
+				moveTo(animPosition.x, animPosition.y, 1f, Interpolation.circleOut),
 				new Action() {
 					@Override
 					public boolean act(float delta) {
@@ -164,19 +208,12 @@ public class LevelCompleteDialog implements ChalkLineAnimationListener {
 							if( number < (scoreItems.size() - 1)  ) {
 								showScoreItem(number + 1);
 							} else {
-								float y = finalYPosition - (2 * padding);
-								ChalkLine line = ChalkLine.create(50f, 
-										y, 
-										350f, 
-										y, 420, LevelCompleteDialog.this);
-								stage.addActor(line);
-								Sounds.drawChalkLine.play();
-								animState = animStates.CHALKLINE_DRAW_BAR;
+								animationController();
 							}
 							return true;
 						}
 					}
-				
+
 				}));
 
 		scoreItem.setActor(scoreItemTable);
@@ -187,88 +224,72 @@ public class LevelCompleteDialog implements ChalkLineAnimationListener {
 		Table table = new Table();
 		table.setHeight(this.maxRowHeight);
 		table.add(new ImageButton(new Image(Textures.bricksVertical).getDrawable()));
-		final Label label = new Label(String.valueOf(this.totalScore), skin);
-		table.add(label).width(50);
+		table.add(totalScoreLabel).width(50);
 
 		table.setPosition(x, -this.rowHeight);
-		
+
 		table.addAction(sequence(
 				moveTo(x, y, 1f, Interpolation.circleOut),
 				new Action() {
 					@Override
 					public boolean act(float delta) {
-						calculateTotalAnimation(x, y, label);
+						animationController();
 						return true;
 					}
 				}));
 
 		stage.addActor(table);
 	}
-	
-	private void calculateTotalAnimation(float x, float y, final Label label) {
-		int size = this.scoreItems.size();
-		for(int i = 0; i < size; i++) {
-			final ScoreItem scoreItem = this.scoreItems.get(i);
-			final Actor actor = scoreItem.getActor();
-			actor.addAction(sequence(moveTo(actor.getX(), y, 1f - (0.1f * i), Interpolation.circleIn),
-					new Action() {
 
-						@Override
-						public boolean act(float delta) {
-							final int amount = scoreItem.getAmount() * scoreItem.getScorePerGameObject();
-							if( amount > 0 ) {
-								Sounds.getSoundForIncrement(amount).play();
-								totalScore += amount;
-								label.setText(String.valueOf(totalScore));
-							}
-							actor.remove();
-							return true;
-						}
-				
-			}));
+	private void calculateTotalAnimation(final int number, final float x, final float y) {
+		ScoreItem scoreItem = this.scoreItems.get(number);
+		final int amount = scoreItem.getAmount() * scoreItem.getAmount() * scoreItem.getScorePerGameObject();
+		final Actor actor = scoreItem.getActor();
+		actor.addAction(sequence(moveTo(actor.getX(), y, 1f - (0.1f * number), Interpolation.circleIn),
+				new Action() {
+
+			@Override
+			public boolean act(float delta) {
+				if( amount > 0 ) {
+					Sounds.getSoundForIncrement(amount).play();
+					totalScore += amount;
+					totalScoreLabel.setText(String.valueOf(totalScore));
+				}
+				actor.remove();
+				if( number >= scoreItems.size() ) {
+					animationController();
+				}
+				return true;
+			}
+
+		}));
+		
+		if( number < (this.scoreItems.size() - 1) ) {
+			calculateTotalAnimation(number + 1, x, y);
 		}
 	}
-	
+
 	private void showTotalScore() {
 		int size = this.chalkLines.size();
 		for(int i = 0; i < size; i++) {
 			this.chalkLines.get(i).addAction(fadeOut(1f));
 		}
-		
+
 	}
-	
+
 	@Override
 	public void onLineDrawEnd(ChalkLine line) {
 		this.chalkLines.add(line);
-		if( this.animState == animStates.CHALKLINE_DRAW_BAR ) {
-			Vector2 v = line.getEnd();
-			float x = v.x + (this.padding * 2);
-			float y = v.y;
-			line = ChalkLine.create(x, y, x + 50f, y, 220, this);
-			this.stage.addActor(line);
-			Sounds.drawChalkLineShort2.play();
-			animState = animStates.CHALKLINE_CROSS_DRAW_HORIZONTAL;
-		} else if ( this.animState == animStates.CHALKLINE_CROSS_DRAW_HORIZONTAL ) {
-			Vector2 v = line.getEnd();
-			float x = v.x - 25f;
-			float y = v.y + (line.getLength()/2f);
-			line = ChalkLine.create(x, y, x, y - 50f, 210, this);
-			this.stage.addActor(line);
-			Sounds.drawChalkLineShort1.play();
-			animState = animStates.CHALKLINE_CROSS_DRAW_VERTICAL;
-		} else if ( this.animState == animStates.CHALKLINE_CROSS_DRAW_VERTICAL ) {
-			Vector2 v = line.getEnd();
-			showCashRegistry(100f, v.y - (this.padding * 2));
-		}
+		animationController();
 	}
-	
-	
+
+
 	private class ScoreItem {
 		private int amount;
 		private int scorePerGameObject;
 		private ImageButton button;
 		private Actor actor;
-		
+
 		public ScoreItem(ImageButton button, int amount, int scorePerGameObject) {
 			this.amount = amount;
 			this.scorePerGameObject = scorePerGameObject;
@@ -287,11 +308,11 @@ public class LevelCompleteDialog implements ChalkLineAnimationListener {
 		public ImageButton getImageButton() {
 			return button;
 		}
-		
+
 		public Actor getActor() {
 			return actor;
 		}
-		
+
 		public void setActor(Actor actor) {
 			this.actor = actor;
 		}
