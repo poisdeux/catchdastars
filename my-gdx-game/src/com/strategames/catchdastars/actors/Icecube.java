a3d3a65e2bba7e3ec9c21ee28cd2eb5aa2414542
Martijn Brekhof <m.brekhof@gmail.com>
2013-06-10 18:03:02 +0200

    * Added edited version of PhysicsBodyLoader to support breakable objects * Implemented drawing parts in Icecube

--- my-gdx-game/src/com/strategames/catchdastars/actors/Icecube.java
+++ my-gdx-game/src/com/strategames/catchdastars/actors/Icecube.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import aurelienribon.bodyeditor.BodyEditorLoader;
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -17,12 +15,12 @@ import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.ContactImpulse;
-import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Json;
 import com.strategames.catchdastars.Game;
+import com.strategames.catchdastars.utils.BodyEditorLoader;
 import com.strategames.catchdastars.utils.ConfigurationItem;
 import com.strategames.catchdastars.utils.Textures;
 
@@ -37,7 +35,6 @@ public class Icecube extends GameObject {
 	private int partsSize;
 
 	public Icecube() {
-		Gdx.app.log("Icecube", "Icecube:");
 		if( availableParts == null ) {
 			setupAvailableParts();
 		}
@@ -50,7 +47,6 @@ public class Icecube extends GameObject {
 	}
 
 	public static Icecube create(World world, float x, float y) {
-		Gdx.app.log("Icecube", "create:");
 		Icecube icecube = new Icecube();
 		icecube.setPosition(x, y);
 		icecube.setWorld(world);
@@ -65,31 +61,21 @@ public class Icecube extends GameObject {
 
 	@Override
 	TextureRegionDrawable createTexture() {
-		Gdx.app.log("Icecube", "createTexture:");
 		for(Part part : this.parts) {
 			Sprite sprite = new Sprite(part.getTexture());
 			sprite.setSize(Game.convertWorldToBox(sprite.getWidth()), Game.convertWorldToBox(sprite.getHeight()));
-			part.setImage(sprite);
+			part.setSprite(sprite);
 		}
 		return new TextureRegionDrawable(Textures.icecube);
 	}
 
 	public void addPart(Part part) {
-		Gdx.app.log("Icecube", "addPart: part="+part);
 		this.parts.add(part);
-		this.partsSize = this.parts.size();
-		Gdx.app.log("Icecube", "addPart: this.partsSize="+this.partsSize);
-	}
-
-	public void removePart(Part part) {
-		this.body.destroyFixture(part.getFixture());
-		this.parts.remove(part);
 		this.partsSize = this.parts.size();
 	}
 
 	@Override
 	Body setupBox2D() {
-		Gdx.app.log("Icecube", "setupBox2D:");
 		World world = getWorld();
 
 		//Balloon body
@@ -99,30 +85,14 @@ public class Icecube extends GameObject {
 		bd.angularDamping = 0.1f;
 		this.body = world.createBody(bd);
 
-		/**
-		 * We need to do this in two steps as we use the BodyEditorLoader
-		 * to attach the fixtures. We need the Fixture in the part object
-		 * to remove it later on when the object breaks. However, the Fixture
-		 * is not returned by the loader. We therefore need to recreate it
-		 * using body.createFixture(FixtureDef) that will return the fixture.
-		 */
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.density = 931f;  // Ice density 0.931 g/cm3 == 931 kg/m3
+		fixtureDef.friction = 0.2f;
+		fixtureDef.restitution = 0.01f; // Make it bounce a little bit
+		
 		for(Part part : this.parts ) {
-			Body tmpBody = world.createBody(bd);
-			FixtureDef fixtureDef = new FixtureDef();
-			fixtureDef.density = 931f;  // Ice density 0.931 g/cm3 == 931 kg/m3
-			fixtureDef.friction = 0.2f;
-			fixtureDef.restitution = 0.01f; // Make it bounce a little bit
-
-			loader.attachFixture(tmpBody, part.getName(), fixtureDef, WIDTH);
-
-			part.setFixtureDef(fixtureDef);
-
-			world.destroyBody(tmpBody);
-		}
-
-		for(Part part : this.parts ) {
-			Fixture f = this.body.createFixture(part.getFixtureDef());
-			part.setFixture(f);
+			loader.attachFixture(this.body, part.getName(), fixtureDef, WIDTH);
+			part.setOrigin(loader.getOrigin(part.getName(), WIDTH).cpy());
 		}
 
 		return this.body;
@@ -130,13 +100,14 @@ public class Icecube extends GameObject {
 
 	@Override
 	public void draw(SpriteBatch batch, float parentAlpha) {
-		setRotation(MathUtils.radiansToDegrees * this.body.getAngle());
+		float rotation = MathUtils.radiansToDegrees * this.body.getAngle();
 		Vector2 v = super.body.getPosition();
-		setPosition(v.x, v.y);
 
 		for(int i = 0; i < this.partsSize; i++) {
-			Sprite sprite = this.parts.get(i).getImage();
+			Part part = this.parts.get(i);
+			Sprite sprite = part.getSprite();
 			sprite.setPosition(v.x, v.y);
+			sprite.setRotation(rotation);
 			sprite.draw(batch, parentAlpha);
 		}
 	}
@@ -149,7 +120,6 @@ public class Icecube extends GameObject {
 
 	@Override
 	public GameObject createCopy() {
-		Gdx.app.log("Icecube", "createCopy");
 		GameObject object = Icecube.create(getWorld(), 
 				getX(), 
 				getY());
@@ -248,30 +218,20 @@ public class Icecube extends GameObject {
 
 	private class Part {
 		String name;
-		Sprite image;
+		Sprite sprite;
 		TextureRegion texture;
-		Fixture fixture;
-		FixtureDef fixtureDef;
-
+		
 		public Part(String name, TextureRegion texture) {
 			this.name = name;
 			this.texture = texture;
 		}
 
-		public void setImage(Sprite image) {
-			this.image = image;
+		public void setSprite(Sprite sprite) {
+			this.sprite = sprite;
 		}
 
-		public Sprite getImage() {
-			return image;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public void setTexture(TextureRegion texture) {
-			this.texture = texture;
+		public Sprite getSprite() {
+			return sprite;
 		}
 
 		public TextureRegion getTexture() {
@@ -281,21 +241,11 @@ public class Icecube extends GameObject {
 		public String getName() {
 			return name;
 		}
-
-		public void setFixture(Fixture fixture) {
-			this.fixture = fixture;
-		}
-
-		public Fixture getFixture() {
-			return fixture;
-		}
-
-		public void setFixtureDef(FixtureDef fixtureDef) {
-			this.fixtureDef = fixtureDef;
-		}
-
-		public FixtureDef getFixtureDef() {
-			return fixtureDef;
+		
+		public void setOrigin(Vector2 origin) {
+			if( sprite != null ) {
+				sprite.setOrigin(origin.x, origin.y);
+			}
 		}
 	}
 }
