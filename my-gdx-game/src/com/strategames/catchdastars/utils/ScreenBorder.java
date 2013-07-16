b96cec9fec78a9339c14b443578ff0e19913c37d
Martijn Brekhof <m.brekhof@gmail.com>
2013-07-16 03:09:09 +0200

    * Fixed size and position of ScreenBorder

--- my-gdx-game/src/com/strategames/catchdastars/utils/ScreenBorder.java
+++ my-gdx-game/src/com/strategames/catchdastars/utils/ScreenBorder.java
@@ -9,19 +9,25 @@ public class ScreenBorder {
 	static public void create(Game game) {
 		float boxWidth = Game.convertWorldToBox(Gdx.graphics.getWidth());
 		float boxHeight = Game.convertWorldToBox(Gdx.graphics.getHeight());
-		Wall wBottom = new Wall(game, 0, 0, boxWidth, Wall.Orientation.HORIZONTAL);
-		game.addGameObject(wBottom);
-		Wall wTop = new Wall(game, 0, boxHeight, boxWidth, Wall.Orientation.HORIZONTAL);
+		
+		Wall wTop = new Wall(game, 0, 0, boxWidth, Wall.Orientation.HORIZONTAL);
+		wTop.increaseSize();
+		wTop.increaseSize();
+		wTop.setPosition(-wTop.getHalfHeight(), boxHeight - wTop.getHalfHeight());
 		game.addGameObject(wTop);
+		
+		Wall wBottom = (Wall) wTop.createCopy();
+		wBottom.setPosition(wTop.getX(), -wBottom.getHalfHeight());
+		game.addGameObject(wBottom);
+		
 		Wall wLeft = new Wall(game, 0, 0, boxHeight, Wall.Orientation.VERTICAL);
 		wLeft.decreaseSize();
 		wLeft.decreaseSize();
-		wLeft.setPosition(wLeft.getX(), wLeft.getY() - wLeft.getStepSize());
+		wLeft.setPosition(-wLeft.getHalfWidth(), wLeft.getY() + wLeft.getHalfWidth());
 		game.addGameObject(wLeft);
-		Wall wRight = new Wall(game, boxWidth, 0, boxHeight, Wall.Orientation.VERTICAL);
-		wRight.decreaseSize();
-		wRight.decreaseSize();
-		wRight.setPosition(wLeft.getX(), wLeft.getY() - wLeft.getStepSize());
+		
+		Wall wRight = (Wall) wLeft.createCopy();
+		wRight.setPosition(boxWidth - wRight.getHalfWidth(), wLeft.getY());
 		game.addGameObject(wRight);
 	}
 }
