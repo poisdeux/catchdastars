8c25f612f5857ba74e1cec59fad9b91f7e8c6f3b
Martijn Brekhof <m.brekhof@gmail.com>
2013-09-27 17:04:07 +0200

    * Added different resolution images for balloons and icecube parts

--- createBackup.sh
+++ createBackup.sh
@@ -1,3 +1,3 @@
 tar czf popdaballoons-gdx.tgz .git 
-mutt -s 'PopDaBalloons gdx version backup' m.brekhof@gmail.com -a popdaballoons-gdx.tgz < /dev/null
-rm popdaballoons-gdx.tgz
+#mutt -s 'PopDaBalloons gdx version backup' m.brekhof@gmail.com -a popdaballoons-gdx.tgz < /dev/null
+#rm popdaballoons-gdx.tgz
