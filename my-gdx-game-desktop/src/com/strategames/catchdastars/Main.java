639da846ed9e52e447c6c3d283656564ec28f460
Martijn Brekhof <m.brekhof@gmail.com>
2013-12-06 20:58:33 +0100

    * Changed to portrait screen for Desktop version

--- my-gdx-game-desktop/src/com/strategames/catchdastars/Main.java
+++ my-gdx-game-desktop/src/com/strategames/catchdastars/Main.java
@@ -8,8 +8,8 @@ public class Main {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "my-gdx-game";
 		cfg.useGL20 = true;
-		cfg.width = 800;
-		cfg.height = 480;
+		cfg.width = 480;
+		cfg.height = 800;
 		
 		new LwjglApplication(new CatchDaStars(), cfg);
 	}
