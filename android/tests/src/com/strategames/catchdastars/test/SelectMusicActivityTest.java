8d877c95d17e26c2f73c9047ada0af426ebd07ec
Martijn Brekhof <m.brekhof@gmail.com>
2014-07-04 21:31:46 +0200

    * Added new tests for testing amount of ListView items and the   content of the items in SelectMusicActivity

--- android/tests/src/com/strategames/catchdastars/test/SelectMusicActivityTest.java
+++ android/tests/src/com/strategames/catchdastars/test/SelectMusicActivityTest.java
@@ -1,9 +1,9 @@
 package com.strategames.catchdastars.test;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 
-import android.database.sqlite.SQLiteDatabase;
-import android.support.v4.app.FragmentManager;
 import android.test.ActivityInstrumentationTestCase2;
 import android.view.View;
 import android.widget.CheckBox;
@@ -19,6 +19,7 @@ import com.strategames.catchdastars.fragments.SelectMusicFragment;
 import com.strategames.engine.musiclibrary.Album;
 import com.strategames.engine.musiclibrary.Artist;
 import com.strategames.engine.musiclibrary.Library;
+import com.strategames.engine.musiclibrary.LibraryItem;
 import com.strategames.engine.musiclibrary.Track;
 
 public class SelectMusicActivityTest extends ActivityInstrumentationTestCase2<SelectMusicActivity> {
@@ -29,82 +30,118 @@ public class SelectMusicActivityTest extends ActivityInstrumentationTestCase2<Se
 	public static final int AMOUNT_OF_ARTISTS = 4;
 	public static final int AMOUNT_OF_ALBUMS = 3;
 	public static final int AMOUNT_OF_TRACKS = 8;
-	
+
 	private ListView listview;
 	private SelectMusicActivity activity;
 	private SelectMusicFragment fragment;
 	private MusicDbHelper dbHelper;
 	private Library library;
-	
+
 	private Solo solo;
-	
+
 	public SelectMusicActivityTest() {
 		super(SelectMusicActivity.class);
+		this.library = createLibrary();
 	}
 
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
 		setActivityInitialTouchMode(false);
-		
+
 		this.activity = getActivity();
 		this.listview = (ListView) this.activity.findViewById(com.strategames.catchdastars.R.id.listview);
 		this.fragment = (SelectMusicFragment) this.activity.getSupportFragmentManager().findFragmentById(R.id.fragment_container);
 		
+		HashMap<String, Artist> artists = library.getArtists();
+		CheckBoxTextViewAdapter adapter = new CheckBoxTextViewAdapter(activity, artists.values().toArray(new Artist[artists.size()]), activity);
+		this.fragment.setAdapter(adapter);
+
 		//Clear database
 		this.dbHelper = new MusicDbHelper(this.activity);
 		this.dbHelper.getWritableDatabase();
 		this.dbHelper.clearDatabase();
-		
+
 		this.solo = new Solo(getInstrumentation(), this.activity);
 	}
 
 	@Override
 	protected void tearDown() throws Exception {
-		super.tearDown();
 		solo.finishOpenedActivities();
+		super.tearDown();
 	}
 
 	public void testPreConditions() {
 		assertNotNull(this.listview);
 		assertNotNull(this.listview.getOnItemClickListener());
 		assertTrue(this.listview.getChildCount() > 0);
-		
 		View view = this.listview.getChildAt(0);
-		assertNotNull(view);
-		assertTrue(view.findViewById(R.id.checkbox) instanceof CheckBox);
-		assertTrue(view.findViewById(R.id.textview) instanceof TextView);
+		assertTrue("ListView item at position 0 is null", view != null);
+		assertTrue("ListView item does not contain a TextView identified by R.id.textview", view.findViewById(R.id.textview) instanceof TextView);
+		assertTrue("ListView item does not contain a CheckBox identified by R.id.checkbox", view.findViewById(R.id.checkbox) instanceof CheckBox);
 		
 		//Check that database is really empty
 		assertTrue(this.dbHelper.getAll().getArtists().isEmpty());
 	}
-	
-	public void testListViewUI() {
-		this.library = createLibrary();
-		
-		this.activity.runOnUiThread(new Runnable() {
-			HashMap<String, Artist> artists = library.getArtists();
-			@Override
-			public void run() {
-				CheckBoxTextViewAdapter adapter = new CheckBoxTextViewAdapter(activity, artists.values().toArray(new Artist[artists.size()]), activity);
-				fragment.setAdapter(adapter);
-			}
-		});
-		
-		
-		testSelectAllForArtist(0);
-		testSelectAllForArtist(3);
-		
-		//Select album 2 for artist 1
+
+	public void testAmountOfItemsShown() {
+		testAmountOfItemsShown(AMOUNT_OF_ARTISTS);
 		solo.clickInList(1);
-		solo.clickOnCheckBox(2);
-		if(solo.isCheckBoxChecked(2)) {
-			fail("Artist1 album2 checkbox not checked");
-		}
-		
-		solo.sleep(5000);
+		testAmountOfItemsShown(AMOUNT_OF_ALBUMS);
+		solo.clickInList(1);
+		testAmountOfItemsShown(AMOUNT_OF_TRACKS);
 	}
 	
+	public void testListViewContentForArtists() {
+		HashMap<String, Artist> artists = this.library.getArtists();
+		LibraryItem[] libraryItems = artists.values().toArray(new LibraryItem[artists.size()]);
+		assertTrue("Library contains "+libraryItems.length+" artists, but should be "+AMOUNT_OF_ARTISTS, libraryItems.length == AMOUNT_OF_ARTISTS);
+		testContentOfItemsShown(libraryItems);
+	}
+	
+	public void testListViewContentForAlbum() {
+		TextView tv = (TextView) this.listview.getChildAt(1).findViewById(R.id.textview);
+		String artistName = tv.getText().toString();
+		
+		HashMap<String, Album> albums = this.library.getArtist(artistName).getAlbums();
+		LibraryItem[] libraryItems = albums.values().toArray(new LibraryItem[albums.size()]);
+		assertTrue("Library contains "+libraryItems.length+" albums, but should be "+AMOUNT_OF_ALBUMS, libraryItems.length == AMOUNT_OF_ALBUMS);
+		
+		//Go into albums view
+		solo.clickInList(1);
+		testContentOfItemsShown(libraryItems);
+	}
+
+	public void testListViewContentForTracks() {
+		TextView tv = (TextView) this.listview.getChildAt(1).findViewById(R.id.textview);
+		String artistName = tv.getText().toString();
+		
+		//Go into albums view
+		solo.clickInList(1);
+		tv = (TextView) this.listview.getChildAt(2).findViewById(R.id.textview);
+		String albumTitle = tv.getText().toString();
+		
+		HashMap<String, Track> tracks = this.library.getArtist(artistName).getAlbum(albumTitle).getTracks();
+		LibraryItem[] libraryItems = tracks.values().toArray(new LibraryItem[tracks.size()]);
+		assertTrue(albumTitle+" for "+artistName+" in library contains "+libraryItems.length+" tracks, but should be "+AMOUNT_OF_TRACKS, libraryItems.length == AMOUNT_OF_TRACKS);
+		
+		//Go into tracks view
+		solo.clickInList(1);
+		testContentOfItemsShown(libraryItems);
+	}
+	
+	public void testSelectArtist() {
+		testSelectAllForArtist(0);
+		testSelectAllForArtist(3);
+	}
+
+	public void testSelectSingleAlbum() {
+		//Select album 2 for artist 1
+		solo.clickInList(1);
+
+		assertTrue("Artist1 album2 checkbox not checked", solo.isCheckBoxChecked(2));
+	}
+
 	private Library createLibrary() {
 		Library library = new Library();
 		for(int i = 0; i < AMOUNT_OF_ARTISTS; i++) {
@@ -120,36 +157,44 @@ public class SelectMusicActivityTest extends ActivityInstrumentationTestCase2<Se
 			}
 			library.addArtist(artist);
 		}
-		
+
 		return library;
 	}
-	
+
 	private void testSelectAllForArtist(int number) {
 		//Select artist
 		solo.clickOnCheckBox(number);
 		//Check if checkbox is selected
-		if(solo.isCheckBoxChecked(number)) {
-			fail("Artist"+number+" checkbox not checked");
-		}
-		
+		assertTrue("Artist"+number+" checkbox not checked", solo.isCheckBoxChecked(number));
+
 		//Check if all albums are selected
 		solo.clickInList(0);
 		for(int j = 0; j < AMOUNT_OF_ALBUMS; j++) {
-			assertTrue(solo.isCheckBoxChecked(j));
-			if(solo.isCheckBoxChecked(number)) {
-				fail("Artist"+number+" album"+j+" checkbox not checked");
-			}
-			
+			assertTrue("Artist"+number+" album"+j+" checkbox not checked", solo.isCheckBoxChecked(j));
+
 			//Check if all tracks are selected
 			solo.clickInList(j);
 			for(int k = 0; k < AMOUNT_OF_TRACKS; k++) {
-				assertTrue(solo.isCheckBoxChecked(k));
-				if(solo.isCheckBoxChecked(number)) {
-					fail("Artist"+number+" album"+j+" track"+k+" checkbox not checked");
-				}
+				assertTrue("Artist"+number+" album"+j+" track"+k+" checkbox not checked", solo.isCheckBoxChecked(k));
 			}
 			solo.goBack();
 		}
 		solo.goBack();
+	}
+	
+	private void testAmountOfItemsShown(int shouldBeAmount) {
+		int childCount = this.listview.getChildCount();
+		assertTrue("Amount of items ("+childCount+") in listview does not equal "+shouldBeAmount, shouldBeAmount == childCount);
+	}
+	
+	private void testContentOfItemsShown(LibraryItem[] items) {
+		int amount = this.listview.getChildCount();
+		for(int i = 0; i < amount; i++) {
+			TextView tv = (TextView) this.listview.getChildAt(i).findViewById(R.id.textview);
+			String listViewItemText = tv.getText().toString();
+			String nameInLibrary = items[i].getName();
+			assertTrue("Content of listview item at position "+i+" is "+listViewItemText+" which does not equal "+nameInLibrary, 
+					nameInLibrary.contentEquals(listViewItemText));
+		}
 	}
 }
